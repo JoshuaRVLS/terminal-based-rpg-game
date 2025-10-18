0210a53cbb6c793d255cde2cea5bfe0a091851cd
Joshua Ganteng <jravaellnew@gmail.com>
2025-10-19 00:41:04 +0700

    Utils

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,7 +1,19 @@
+#pragma once
+#include <chrono>
 #include <iostream>
+#include <random>
 
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <termios.h>
+#include <unistd.h>
+#endif
 namespace utils {
 
 void clear_screen();
-
-}
+#ifdef __linux__
+char getch();
+#endif
+double generateNumber(int lowerBound, int higherBound);
+} // namespace utils
