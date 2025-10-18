f6caa5d41771999d73319f7fa4026b84bd4601d8
Joshua Ganteng <jravaellnew@gmail.com>
2025-10-19 00:41:10 +0700

    Player class

--- include/player.hpp
+++ include/player.hpp
@@ -4,17 +4,32 @@
 #include "weapon.hpp"
 #include <memory>
 
+enum PlayerClass { KNIGHT, MAGE, SPEARMAN };
+
 class Player : public Sprite {
 
+  std::string name;
+  PlayerClass player_class;
   int player_coins;
   std::vector<std::unique_ptr<Weapon>> player_weapons;
   std::vector<std::unique_ptr<Item>> player_items;
 
+  void init();
+
 public:
-  Player(const std::string &display_value, const Position &position);
+  Player(const std::string &display_value, const PlayerClass &player_class,
+         const Position &position);
+
+  PlayerClass &get_class();
 
   void attack(Sprite &target, Weapon &weapon) override;
   void use_skill(const int &skill_id) override;
   void use_item(const uint8_t &item_id) override;
   void guard() override;
+
+  void set_name(const std::string &name);
+  void set_player_coins(const int &player_coins);
+
+  std::string &get_name();
+  int &get_coins();
 };
