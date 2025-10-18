bb8ee95c9537f07abcc0af0478b1833458c657c8
Joshua Ganteng <jravaellnew@gmail.com>
2025-10-19 01:49:31 +0700

    Refactor Game class: add battle-related methods and member variables

--- include/game.hpp
+++ include/game.hpp
@@ -1,17 +1,22 @@
 #pragma once
 #include "environments/dirt.hpp"
 #include "environments/rock.hpp"
+#include "monster.hpp"
 #include "object.hpp"
 #include "player.hpp"
+#include "skill.hpp"
+#include "weapon.hpp"
 #include <iostream>
 #include <memory>
 #include <vector>
 
-class Game {
 
+class Game {
   bool running = true;
   bool game_start = false;
+  bool in_battle = false;
   Player *player;
+  Monster *current_monster = nullptr;
   std::vector<std::vector<std::vector<std::unique_ptr<Object>>>> maps;
 
   void init();
@@ -21,8 +26,18 @@ class Game {
   void render_map();
   void generate_player(PlayerClass player_class);
   void handle_movement();
+  void handle_battle_input();
   void main_menu();
   void render_info();
+  void render_battle();
+  void start_battle(Monster &monster);
+  void end_battle(bool player_won);
+  void player_attack();
+  void player_use_skill();
+  void player_use_item();
+  void player_guard();
+  void monster_turn();
+  void check_battle_end();
 
 public:
   Game();
