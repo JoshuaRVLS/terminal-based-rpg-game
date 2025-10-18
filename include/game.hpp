abb9e3197a1cd6cba33140d9a6047da437999c1f
Joshua Ganteng <jravaellnew@gmail.com>
2025-10-18 23:22:24 +0700

    Added player and generate_player

--- include/game.hpp
+++ include/game.hpp
@@ -2,13 +2,16 @@
 #include "environments/dirt.hpp"
 #include "environments/rock.hpp"
 #include "object.hpp"
+#include "player.hpp"
 #include <iostream>
 #include <memory>
 #include <vector>
 
+
 class Game {
 
   bool running = true;
+  std::unique_ptr<Player> player;
   std::vector<std::vector<std::vector<std::unique_ptr<Object>>>> maps;
 
   void init();
@@ -16,6 +19,7 @@ class Game {
   void render();
   void generate_map();
   void render_map();
+  void generate_player();
 
 public:
   Game();
