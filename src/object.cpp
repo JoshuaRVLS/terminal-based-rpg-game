c2fb59b31f5a135f88a0b8defa4801cb1051e2a6
Joshua Ganteng <jravaellnew@gmail.com>
2025-10-18 23:23:48 +0700

    Object class as parent for every object ingame

--- src/object.cpp
+++ src/object.cpp
@@ -1,5 +1,6 @@
 #include "object.hpp"
 
-Object::Object(const Position &position) : position(position) {}
+Object::Object(const std::string &display_value, const Position &position)
+    : display_value(display_value), position(position) {}
 
 Position &Object::get_position() { return position; }
