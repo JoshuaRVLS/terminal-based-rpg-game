3333144a9a47519ece5355a141fdeed0bb0cd2c6
Joshua Ganteng <jravaellnew@gmail.com>
2025-10-18 23:54:44 +0700

    remove get_value for environment, using from Object class

--- include/environment.hpp
+++ include/environment.hpp
@@ -4,12 +4,8 @@
 
 class Environment : public Object {
 
-  std::string value;
-
 public:
-  Environment(const std::string &value, const Position &position);
-
-  std::string &get_value();
+  Environment(const std::string &display_value, const Position &position);
 
   std::string getType() const override { return "Environment"; }
 };
