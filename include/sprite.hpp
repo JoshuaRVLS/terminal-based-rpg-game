7b1067328257544eb81998e764451eea2f3ce802
Joshua Ganteng <jravaellnew@gmail.com>
2025-10-19 01:49:57 +0700

    Refactor Sprite class: update includes, add skill management methods, and improve destructor declaration

--- include/sprite.hpp
+++ include/sprite.hpp
@@ -1,21 +1,24 @@
 #pragma once
+#include "object.hpp"
 #include <iostream>
-#include <object.hpp>
+#include <memory>
 #include <vector>
 
 class Weapon;
 class Item;
+class Skill;
 
 class Sprite : public Object {
 protected:
   int sprite_health;
   int sprite_stamina;
   int sprite_shield;
+  std::vector<std::unique_ptr<Skill>> skills;
 
 public:
   Sprite(const std::string &display_value, const int &health,
          const int &stamina, const int &shield, const Position &position);
-  ~Sprite() = default;
+  virtual ~Sprite() = default;
 
   int &get_sprite_health();
   int &get_sprite_stamina();
@@ -24,6 +27,12 @@ public:
   void set_sprite_health(const int &sprite_health);
   void set_sprite_stamina(const int &sprite_stamina);
   void set_sprite_shield(const int &sprite_shield);
+
+  void add_skill(std::unique_ptr<Skill> skill);
+  void remove_skill(int index);
+  Skill *get_skill(int index);
+  const std::vector<std::unique_ptr<Skill>> &get_skills() const;
+  int get_skill_count() const;
 
   virtual void attack(Sprite &target, Weapon &weapon) = 0;
   virtual void use_skill(const int &skill_id) = 0;
