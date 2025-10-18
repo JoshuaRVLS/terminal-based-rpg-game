33e19da3b019fe130cd5180b22006b8764f38842
Joshua Ganteng <jravaellnew@gmail.com>
2025-10-19 00:40:52 +0700

    utils. detect linux for getch

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,3 +7,29 @@ void utils::clear_screen() {
   system("cls");
 #endif
 }
+
+double utils::generateNumber(int lowerBound, int higherBound) {
+  std::default_random_engine randomEngine(
+      std::chrono::system_clock::now().time_since_epoch().count());
+  std::uniform_real_distribution<double> generateNumber(lowerBound,
+                                                        higherBound);
+  return generateNumber(randomEngine);
+}
+
+#ifdef __linux__
+char utils::getch() {
+  struct termios oldt;
+  tcgetattr(STDIN_FILENO, &oldt);
+
+  struct termios newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+  char ch;
+  read(STDIN_FILENO, &ch, 1);
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+  return ch;
+}
+#endif
